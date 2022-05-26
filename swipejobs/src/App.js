dd047d75c71b70de0c4c8d23057930d967c07512
VincensiuD <spanishfrenchromanian@gmail.com>
2022-05-27 01:17:51 +1000

    commit initial components and initial fetch of worker details

--- swipejobs/src/App.js
+++ swipejobs/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
 
 function App() {
+
+const workerId = "7f90df6e-b832-44e2-b624-3143d428001f";
+
+// const [jobTitle,setJobTitle] = useState({});
+// const [company,setCompany] = useState({});
+const [wageInCents,setWageInCents] = useState(0);  
+const [workerName, setWorkerName] = useState(""); 
+
+useEffect(
+  () => {
+    const fetchedData = async () => {
+    
+      const response = await
+       fetch(`https://test.swipejobs.com/api/worker/${workerId}/profile`);
+      
+       const data = await response.json();
+
+       const fullName = data.firstName + " " + data.lastName;
+
+       setWorkerName(fullName);
+   
+
+    };
+  
+    fetchedData()
+      .catch(console.error);
+  }, []);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Test</p>
+      <p>{workerName}</p>
     </div>
   );
 }
